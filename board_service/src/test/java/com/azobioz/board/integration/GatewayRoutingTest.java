0c9b8e84302c7298e2507bbb47e8bfeb00586269
Azobioz <aziz345568667@gmail.com>
2026-03-13 17:23:09 +0300

    commit fix tests

--- board_service/src/test/java/com/azobioz/board/integration/GatewayRoutingTest.java
+++ board_service/src/test/java/com/azobioz/board/integration/GatewayRoutingTest.java
@@ -8,10 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class GatewayRoutingTest {
 
     @LocalServerPort
